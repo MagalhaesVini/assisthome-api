f6120dc5f329a6b1574cda9f2990a2e209c275a7
Vinícius Magalhães <vinizipi@gmail.com>
2026-08-10 19:57:46 -0300

    chore: initialize AssistHome API

---
+++ src/main/java/com/assisthome/AssistHomeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.assisthome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistHomeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssistHomeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/assisthome/AssistHomeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assisthome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistHomeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
